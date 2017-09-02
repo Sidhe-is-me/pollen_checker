555924639507c6e625341f1beeb59a08eed4366a
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-09-02 19:48:18 +0000

    refactor more info method into a menu method

--- lib/pollen_checker/cli.rb
+++ lib/pollen_checker/cli.rb
@@ -6,29 +6,50 @@ class PollenChecker::CLI
   def call
     pollen_zip_code
     list_pollen_level
-    more_info
+    menu
   end
 
 def pollen_zip_code
   puts "Enter the zip code you
-   would like the pollen level checked for:"
+  would like the pollen level checked for:"
   zipcode = gets.strip
-  # zipcode must be only 5 numbers
-  puts zipcode
+  # zipcode must be only 5 numbers and a valid zipcode
+  #this will take the zipcode and check it against the pollen.com website scraper
 end
 
-
-  def list_pollen_level
+def list_pollen_level
+  #this will scrape the pollen levle info from pollen.com
     pollen_level = 0
     puts "Today's pollen level is #{pollen_level}"
-  end
-
-def more_info
-  puts "Would you like to know the type of pollen
-  in the area? (y/n)"
-  #if yes then scrape for the pollen types if no then
-  #ask if they want to input a new zip or exit.
 end
 
 
+def menu
+  puts "What would you like to do:
+  1. Would you like to know the type of pollen(s)in the area?
+  2. Check another zipcode
+  or exit"
+  input = nil
+  while input != "exit"
+    input = gets.strip.downcase
+    case input
+    when "1"
+      #scrape 2nd layer of pollen.com for pollen types
+            pollen_type = "ragweed"
+            puts  "The type of pollen in the area is #{pollen_type}, don't forget
+            to take your Quercitin!"
+            call
+    when "2"
+      call
+    end
+end
+goodby
+end
+
+
+def goodby
+  puts "Goodby! See you tomorrow for another check in!"
+end
+
+#class end
 end
