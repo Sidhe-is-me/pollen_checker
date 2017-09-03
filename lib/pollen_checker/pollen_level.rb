5b1955c16493d1e07c50a8d39900757bbbdaeaa8
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-09-03 20:37:25 +0000

    interpolate zipcode into url,move self.today into scraper menthod

--- lib/pollen_checker/pollen_level.rb
+++ lib/pollen_checker/pollen_level.rb
@@ -2,17 +2,20 @@ class PollenChecker::Pollen_level
   attr_accessor :current_level, :history, :future, :top_allergens, :url
 
 def self.today
+  self.scrape_pollen_levels
+end
+
+def self.scrape_pollen_levels
+  # PollenChecker::Pollen_level_scraper.new("https://www.pollen.com/forecast/current/pollen/#{zipcode}")
   zipcode = self.new
   zipcode.current_level = "8"
   zipcode.history = "Yesterday  Pollen Level insert history"
-  # for some reason the irb does not like zipcode.future or forecast
   zipcode.future = "Tomorrow  Pollen level  insert forecast"
   zipcode.top_allergens = ['Ragweed','Chenopods','Nettel']
-  zipcode.url = 'https://www.pollen.com/forecast/current/pollen/'
+  zipcode.url = 'https://www.pollen.com/forecast/current/pollen/#{zipcode}'
   [zipcode]
+
 end
-
-
 
 
 
