b411e1d5d5ac48bb54055627fde1fcbbc29562f9
github username <github email address>
2017-09-03 20:54:09 -0400

    update dependancies pry and nokogiri

--- lib/pollen_checker.rb
+++ lib/pollen_checker.rb
@@ -1,7 +1,8 @@
 
 #this file acts as my environment
 # require 'open-uri'
-
+require "pry"
+require "nokogiri"
 
 require_relative "pollen_checker/version"
 require_relative "pollen_checker/pollen_level"
