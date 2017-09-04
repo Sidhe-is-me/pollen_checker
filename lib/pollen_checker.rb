3b14d4ed1264008f2d7de1e904c6af1eb8f55640
github username <github email address>
2017-09-03 20:38:22 -0400

    Testing after going local

--- lib/pollen_checker.rb
+++ lib/pollen_checker.rb
@@ -1,8 +1,7 @@
 
 #this file acts as my environment
 # require 'open-uri'
-# require 'pry'
-# require 'nokogiri'
+
 
 require_relative "pollen_checker/version"
 require_relative "pollen_checker/pollen_level"
